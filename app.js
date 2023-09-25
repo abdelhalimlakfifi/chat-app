9ccd77d77834371a5dd2eb799310d4103cfead68
abdelhalimlakfifi <abdelhalimlakfifi@gmail.com>
2023-09-25 14:27:29 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 4000;
+const server = app.listen(PORT,() => console.log(`Listening on port: ${PORT}`));
+
+
+const io = require('socket.io')(server);
+
+
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+io.on('connection', (socket) => {
+    console.log(socket.id);
+})
